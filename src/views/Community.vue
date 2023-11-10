ad551537138aeef7199a1b37ba92a168d06d3c57
avcoder <avcoder@users.noreply.github.com>
2023-11-10 07:29:02 -0500

    add hero to community page

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -3,7 +3,14 @@
 <template>
     <div class="grid">
         <div class="col-12">
-            <div class="card"></div>
+            <div class="surface-section px-4 py-8 md:px-6 lg:px-8">
+                <div class="text-700 text-center">
+                    <div class="text-blue-600 font-bold mb-3"><i class="pi pi-discord"></i>&nbsp;POWERED BY DISCORD</div>
+                    <div class="text-900 font-bold text-5xl mb-3">Join Our AA Community</div>
+                    <div class="text-700 text-2xl mb-5">Gain knowledge through the collection of conversation topics and frequently asked questions.</div>
+                    <Button label="Join Now" icon="pi pi-discord" class="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap"></Button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
